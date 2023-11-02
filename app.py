c01ef46ad2bc759e48dba9c2701efe1b998d05cd
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-11-02 11:25:18 +0530

    workd

--- app.py
+++ app.py
@@ -1,70 +1,57 @@
 from fastapi import FastAPI, UploadFile, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from logging.config import dictConfig
 import time
 import random
 import subprocess
-import logging
+import base64
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
-log_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "%(levelprefix)s %(asctime)s %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-    },
-    "handlers": {
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr",
-        },
-    },
-    "loggers": {
-        "foo-logger": {"handlers": ["default"], "level": "DEBUG"},
-    },
-}
-dictConfig(log_config)
-
-logger = logging.getLogger("foo-logger")
 app = FastAPI(
     debug=True,
 )
-
-
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    logger.debug(f"{process_time =}")
-    return response
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def is_image(content_type: str) -> bool:
-    return content_type.find("image")
-    """ return any( """
-    """     [content_type.find(ext)  """
-    """      for ext in ["png", "jpeg", "jpg", "gif", "webp"]] """
-    """ ) """
+    return any(
+        [content_type.find(ext) for ext in ["png", "jpeg", "jpg", "gif", "webp"]]
+    )
 
 
 @app.post("/scale")
-async def image_scale(file: UploadFile, scale):
+async def image_scale(file: UploadFile):
+    scale = 30
+    filename = file.filename.replace(" ", "_")
+
+    print(f"{file.content_type=}")
+    print(f"{filename=}")
+
     if not is_image(file.content_type):
+        print("not image")
         return "notimage"
-    with open(f"./scratch/{file.filename}", "wb+") as fp:
+
+    with open(f"./scratch/{filename}", "wb+") as fp:
         fp.write(file.file.read())
-    time.sleep(random.random() * random.randint(1, 3))
+
+    """ time.sleep(random.random() * random.randint(1, 3)) """
     cmd = [
         "convert",
-        "./scratch/" + file.filename,
+        "./scratch/" + filename,
         f"-resize {scale}%",
-        "./scaled/scaled_" + file.filename,
+        "./scaled/scaled_" + filename,
     ]
     _p: str = (
         subprocess.Popen(
@@ -75,6 +62,12 @@ async def image_scale(file: UploadFile, scale):
         .communicate()[0]
         .decode()
     )
-    time.sleep(random.random() * random.randint(1, 2))
+    """ time.sleep(random.random() * random.randint(1, 2)) """
 
-    return FileResponse(f"./scaled/scaled_{file.filename}")
+    with open(f"./scaled/scaled_{filename}", "rb") as fp:
+        return {
+            "image": base64.b64encode(fp.read()),
+            "type": file.content_type,
+        }
+
+    """ return FileResponse(f"./scaled/scaled_{filename}") """
