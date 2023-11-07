b294c096031eca59ba36acca2124e90d963ac986
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-11-07 19:12:53 +0530

    Working

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from fastapi import FastAPI, UploadFile, Request
+from fastapi import FastAPI, UploadFile, Request, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+import requests
 import time
 import random
 import subprocess
 import base64
+import uuid
+import logging
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
 
+STORAGE_SERVICE_URL = "http://localhost:4000/api/files/upload",
+
+logger = logging.getLogger("uvicorn.error")
+logger.info("Logger Up")
 app = FastAPI(
     debug=True,
 )
@@ -32,43 +33,52 @@ def is_image(content_type: str) -> bool:
 
 
 @app.post("/scale")
-async def image_scale(file: UploadFile):
-    scale = 30
-    filename = file.filename
-
-    print(f"{file.content_type=}")
-    print(f"{filename=}")
+async def image_scale(file: UploadFile, scale: int, background_tasks: BackgroundTasks):
+    filename = file.filename.replace(" ", "_")
 
     if not is_image(file.content_type):
         print("not image")
         return "notimage"
-
+    original_file = file.file.read()
     with open(f"./scratch/{filename}", "wb+") as fp:
-        fp.write(file.file.read())
+        fp.write(original_file)
 
-    """ time.sleep(random.random() * random.randint(1, 3)) """
+    time.sleep(random.random() * random.randint(1, 3))
     cmd = [
         "convert",
         "./scratch/" + filename,
         f"-resize {scale}%",
         "./scaled/scaled_" + filename,
     ]
-    _p: str = (
-        subprocess.Popen(
-            " ".join(cmd),
-            stdout=subprocess.PIPE,
-            shell=True,
-        )
-        .communicate()[0]
-        .decode()
+
+    cmd = f"convert ./scratch/{filename} -resize {scale}% ./scaled/scaled_{filename}"
+
+    subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        shell=True,
     )
-    """ time.sleep(random.random() * random.randint(1, 2)) """
+    time.sleep(random.random() * random.randint(1, 2))
+
+    def send_images_to_storage():
+        logger.info("Sending image to Storage service")
+        requests.post(
+            STORAGE_SERVICE_URL,
+            json={
+                "scale": scale,
+                "file": base64.b64encode(original_file).decode(),
+                "user_id": "502b2c72-fd13-4c1d-a9e1-f0e82c8b7459",
+                "filename": filename,
+            },
+        )
+        logger.info("Sent image to Storage service")
 
     with open(f"./scaled/scaled_{filename}", "rb") as fp:
-
+        background_tasks.add_task(
+            send_images_to_storage,
+        )
         return {
             "image": base64.b64encode(fp.read()),
             "type": file.content_type,
         }
 
-    """ return FileResponse(f"./scaled/scaled_{filename}") """
