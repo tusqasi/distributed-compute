62dc926cf264f8bbeaff55b3a21f31adc9e4d593
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-11-08 20:00:19 +0530

    Update

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from fastapi import FastAPI, UploadFile, Request, BackgroundTasks
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-import requests
-import time
-import random
-import subprocess
 import base64
-import uuid
 import logging
+import os
+import subprocess
+import uuid
 
+import requests
+from fastapi import BackgroundTasks, FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
-STORAGE_SERVICE_URL = "http://localhost:4000/api/files/upload",
+STORAGE_SERVICE_URL = "http://localhost:4000/api/files/upload"
 
 logger = logging.getLogger("uvicorn.error")
 logger.info("Logger Up")
@@ -27,53 +25,54 @@ app.add_middleware(
 
 
 def is_image(content_type: str) -> bool:
-    return any(
-        [content_type.find(ext) for ext in ["png", "jpeg", "jpg", "gif", "webp"]]
-    )
+    return any(content_type.find(ext) for ext in ["png", "jpeg", "jpg", "gif", "webp"])
 
 
 @app.post("/scale")
-async def image_scale(file: UploadFile, scale: int, background_tasks: BackgroundTasks):
+async def image_scale(
+    file: UploadFile, scale: int, user_id: str, background_tasks: BackgroundTasks
+):
     filename = file.filename.replace(" ", "_")
 
+    # I am testing this setup, i am uploading a single file multiple times which is not expected
+    # having randomess in the filename will alleviate conflicts
+    random_id = uuid.uuid4()
     if not is_image(file.content_type):
         print("not image")
         return "notimage"
+
     original_file = file.file.read()
-    with open(f"./scratch/{filename}", "wb+") as fp:
+
+    with open(f"./scratch/{random_id}{filename}", "xb") as fp:
         fp.write(original_file)
 
-    time.sleep(random.random() * random.randint(1, 3))
-    cmd = [
-        "convert",
-        "./scratch/" + filename,
-        f"-resize {scale}%",
-        "./scaled/scaled_" + filename,
-    ]
+    cmd = f"convert ./scratch/{random_id}{filename} -resize {scale}% ./scaled/{random_id}{filename}"
 
-    cmd = f"convert ./scratch/{filename} -resize {scale}% ./scaled/scaled_{filename}"
-
-    subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        shell=True,
-    )
-    time.sleep(random.random() * random.randint(1, 2))
+    subprocess.run(cmd, shell=True)
 
     def send_images_to_storage():
         logger.info("Sending image to Storage service")
-        requests.post(
-            STORAGE_SERVICE_URL,
-            json={
-                "scale": scale,
-                "file": base64.b64encode(original_file).decode(),
-                "user_id": "502b2c72-fd13-4c1d-a9e1-f0e82c8b7459",
-                "filename": filename,
-            },
-        )
-        logger.info("Sent image to Storage service")
+        try:
+            ## CUrl is just *chefs-kiss*
+            res = subprocess.run(
+                f"curl -q -X POST \
+                    -F 'file=@./scratch/{random_id}{filename}' \
+                    'http://127.0.0.1:4000/api/files/upload?scale={scale}&user_id={user_id}' ",
+                shell=True,
+                check=True,
+                capture_output=True,
+            )
+            logger.info("Sent image to Storage service: %s ", res.stdout.decode())
+        except requests.exceptions.ConnectionError:
+            logger.error(
+                "Can not connect to File Storage Service. Is the service running?"
+            )
+        finally:
+            logger.info("Removing temp files")
+            os.remove(f"./scratch/{random_id}{filename}")
+            os.remove(f"./scaled/{random_id}{filename}")
 
-    with open(f"./scaled/scaled_{filename}", "rb") as fp:
+    with open(f"./scaled/{random_id}{filename}", "rb") as fp:
         background_tasks.add_task(
             send_images_to_storage,
         )
@@ -81,4 +80,3 @@ async def image_scale(file: UploadFile, scale: int, background_tasks: Background
             "image": base64.b64encode(fp.read()),
             "type": file.content_type,
         }
-
